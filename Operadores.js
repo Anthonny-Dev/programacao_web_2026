650c9ad53972916d504cac0482dcbe4cf5aaa10c
Anthonny-Dev <144443225+Anthonny-Dev@users.noreply.github.com>
2026-03-08 23:01:28 -0300

    Add files via upload

---
+++ Operadores.js
@@ -0,0 +1,34 @@
+a = 5;
+b = 10;
+
+
+function soma(){
+    var c = a + b; 
+    console.log(`Resultado da operação soma entre ${a} e ${b} é: `, c)
+}
+
+soma()
+
+
+function subtracao(){
+    var c = a - b; 
+    console.log(`Resultado da operação subtração entre ${a} e ${b} é: `, c)
+}
+
+subtracao()
+
+
+function multiplacacao(){
+    var c = a * b; 
+    console.log(`Resultado da operação multiplacacao entre ${a} e ${b} é: `, c)
+}
+
+multiplacacao()
+
+
+function divisao(){
+    var c = a / b; 
+    console.log(`Resultado da operação divisão entre ${a} e ${b} é: `, c)
+}
+
+divisao()
