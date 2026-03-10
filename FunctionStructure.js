ecb4b66bc79604b8800f21b992e6fd382885fca9
Anthonny-Dev <anthonnyvitoriano23@gmail.com>
2026-03-10 04:20:24 -0300

    adicionando atividade2

---
+++ FunctionStructure.js
@@ -0,0 +1,21 @@
+// Function - bloco de código que pode ser acionado
+// DRY - Dont Repeat YourSelf 
+
+function imprimirOlaMundo(){
+    console.log("Olá mundo!");
+}
+
+imprimirOlaMundo(); // chamada de função
+
+
+function somar(a, b){
+    let soma = a + b;
+
+    console.log(soma);
+}
+
+somar(10, 5);
+somar(15, 7);
+somar(1, 5);
+somar(10, 9);
+somar(2, 7);
---
+++ atividade2/verificarNumeroPrimo.js
@@ -0,0 +1,27 @@
+function verificarNumeroPrimo(n) {
+
+    if (n <= 1) {
+        console.log(`${n} não é primo`);
+        return;
+    }
+
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            console.log(`${n} não é primo`);
+            return;
+        }
+    }
+
+    console.log(`${n} é primo`);
+}
+
+verificarNumeroPrimo(0); //false
+verificarNumeroPrimo(1); //false
+verificarNumeroPrimo(2); //true
+verificarNumeroPrimo(3); //true
+verificarNumeroPrimo(7); //true
+verificarNumeroPrimo(83); //true
+verificarNumeroPrimo(100); //false
+verificarNumeroPrimo(991); //true
+verificarNumeroPrimo(104729); //true
+verificarNumeroPrimo(14348907); //false
